78cf85f08a55019ffe1a391d05b86c3022efb57f
Elene Akhvlediani <eakhv11@freeuni.edu.ge>
2017-08-09 15:12:16 +0400

    classes

---
+++ GalleryLS/Classes/LSGallery.h
@@ -0,0 +1,19 @@
+//
+//  LSGallery.h
+//  LSGallery
+//
+//  Created by Elene Akhvlediani on 8/4/17.
+//  Copyright © 2017 Elene Akhvlediani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LSGallery.
+FOUNDATION_EXPORT double LSGalleryVersionNumber;
+
+//! Project version string for LSGallery.
+FOUNDATION_EXPORT const unsigned char LSGalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSGallery/PublicHeader.h>
+
+
---
+++ GalleryLS/Classes/LSGallery.swift
@@ -0,0 +1,20 @@
+//
+//  tets.swift
+//  LSGallery
+//
+//  Created by Elene Akhvlediani on 8/4/17.
+//  Copyright © 2017 Elene Akhvlediani. All rights reserved.
+//
+
+import Foundation
+
+
+public class SwiftFrameworks {
+    public init (){
+        print("Class has been initialised")
+    }
+    
+   public func doSomething(){
+    print("Yeah, it works")
+    }
+}
